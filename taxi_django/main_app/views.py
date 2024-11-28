ef1f64190b26aef4b610362cae37e5d3649ba35c
MrsViktoRs <Vhomutov167@gmail.com>
2024-11-28 13:01:11 +0300

    add_send_mess

--- taxi_django/main_app/views.py
+++ taxi_django/main_app/views.py
@@ -1,8 +1,10 @@
+import requests
 from rest_framework import generics, status
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import ListAPIView
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views import View
 from rest_framework.decorators import action
 
 from .models import *
@@ -74,13 +76,32 @@ class AppealsView(generics.UpdateAPIView):
 @csrf_exempt
 def get_user_status(request):
     if request.method == 'GET':
-        phone = request.GET.get('phone')
-        if phone:
-            try:
-                user = Users.objects.get(phone=phone)
-                serializer = UserSerializer(user)
-                return JsonResponse({'res_status': serializer.data['res_status']})
-            except Users.DoesNotExist:
-                return JsonResponse({'status': 'not_found'}, status=404)
+        try:
+            user = Users.objects.filter(res_status=False)
+            serializer = UserSerializer(user, many=True)
+            return JsonResponse(serializer.data, safe=False)
+        except Users.DoesNotExist:
+            return JsonResponse({'status': 'not_found'}, status=404)
+
+
+class SendMessageView(View):
+    def post(self, request, *args, **kwargs):
+        chat_id = request.GET.get('chat_id')
+        message = 'тест'
+
+        if not message:
+            return JsonResponse({'error': 'Message content cannot be empty.'}, status=400)
+
+        token = '7754471910:AAGJ0T8CHy6MP4CsSr-pkqU0syYAHXWeX04'
+        url = f'https://api.telegram.org/bot{token}/sendMessage'
+        payload = {
+            'chat_id': chat_id,
+            'text': message
+        }
+
+        response = requests.post(url, json=payload)
+
+        if response.status_code == 200:
+            return JsonResponse({'status': 'Message sent successfully!'})
         else:
-            return JsonResponse({'error': 'phone parameter is required'}, status=400)
+            return JsonResponse({'error': 'Failed to send message.'}, status=response.status_code)
