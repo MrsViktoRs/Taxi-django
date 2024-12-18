d2e147e3142f8860f95f4290354be1f594ede962
MrsViktoRs <Vhomutov167@gmail.com>
2024-12-18 17:47:37 +0300

    fix_requirements-v2

--- taxi_django/taxi_django/settings.py
+++ taxi_django/taxi_django/settings.py
@@ -156,10 +156,10 @@ LOGGING = {
     # },
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://localhost:6379/2'
 REDIS_HOST = 'localhost'
 REDIS_PORT = '6379'
-BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/2'
 BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
 CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 
